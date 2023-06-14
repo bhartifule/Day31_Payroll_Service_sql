aee30f8040018058018e14d20f8a6c299b1b6d47
bhartifule <116824470+bhartifule@users.noreply.github.com>
2023-06-15 00:40:46 +0530

    Update Main.java

--- bridglabz/sql/Main.java
+++ bridglabz/sql/Main.java
@@ -2,6 +2,6 @@ package com.bridglabz.sql;
 
 public class Main {
 public static void main(String[] args) {
-	System.out.println("WellCome Employee Payroll Program");
+	System.out.println("WellCome Employee Payroll Program"); 
 }
 }
